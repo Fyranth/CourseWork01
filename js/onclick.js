22ebe1a925641d79640ffa0cb5b4937c1c1ebe5f
Fyranth <glebw@gmail.com>
2020-04-16 14:49:42 +0300

    js emulate for ios

---
+++ js/onclick.js
@@ -0,0 +1,14 @@
+function mm_open () {
+
+    var Elem = document.getElementsByClassName('mm_ul');
+    Elem.Style.position = absolute;
+    Elem.Style.display= flex;
+    Elem.Style.justify-content = right;
+    Elem.Style.flex-direction = column;
+    Elem.Style.left= 40 px;
+    Elem.Style.right= 0 px;
+    Elem.Style.z-index= 5;
+    Elem.Style.width= 150 px;
+    Elem.Style.margin-left= 0 px;
+
+}
