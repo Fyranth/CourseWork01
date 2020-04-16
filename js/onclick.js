c28d9b6fb57e51767c78b58f7af1eaa759543061
Fyranth <glebw@gmail.com>
2020-04-16 21:42:38 +0300

    полпути

--- js/onclick.js
+++ js/onclick.js
@@ -1,14 +1,30 @@
-function mm_open () {
+function mm_open (event, classname) {
+    var target = event.target;
+    var par = target.offsetParent;
+    var elemCol = par.getElementsByClassName(classname);
+    var elem = elemCol[0];
+    var elemStyle = elem.style;
+    elemStyle.display = 'flex';
+    elemStyle.position = 'absolute';
+    
+    elemStyle.left = '40px';
+    elemStyle.right = '0px';
+    elemStyle.width = '150px';
+    elemStyle.margin = '3px 0px 3px 0px;'
 
-    var Elem = document.getElementsByClassName('mm_ul');
-    Elem.Style.position = absolute;
-    Elem.Style.display= flex;
-    Elem.Style.justify-content = right;
-    Elem.Style.flex-direction = column;
-    Elem.Style.left= 40 px;
-    Elem.Style.right= 0 px;
-    Elem.Style.z-index= 5;
-    Elem.Style.width= 150 px;
-    Elem.Style.margin-left= 0 px;
+}
+
+function mm_open_child (event, classname) {
+    var target = event.target;
+    var par = target.offsetParent;
+    var elemCol = par.getElementsByClassName(classname);
+    var elem = elemCol[0];
+    var elemStyle = elem.style;
+    elemStyle.display = 'flex';
+    elemStyle.position = 'absolute';
+    elemStyle.left = '40px';
+    elemStyle.right = '0px';
+    elemStyle.width = '150px';
+    elemStyle.margin = '3px 0px 3px 0px;'
 
 }
